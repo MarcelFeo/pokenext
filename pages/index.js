e290f744b8748bf86ffa9f59e33c9da0da375ba5
MarcelFeo <marcelfeo29@gmail.com>
2022-01-18 14:50:30 -0300

    conectando a api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const maxPokemon = 151;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const res = await fetch(`${api}/?limit=${maxPokemon}`);
+  const data = await res.json();
+
+  // add pokemon id
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  })
+
+  return {
+    props: {
+      pokemon: data.results,
+    },
+  }
+
+}
+
+export default function Home({ pokemon }) {
   return (
-    <h1>PokeNext</h1>
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemon.map((pokemon) =>(
+          <li key={pokemon.id}>{pokemon.id} | {pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
